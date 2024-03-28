17950f294e62130f7e631805fffe0a338d9c3ff2
Shaun Martin <inhumantsar@protonmail.com>
2024-03-28 12:09:57 -0500

    fix: address review comments

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, ButtonComponent, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, ProgressBarComponent, Setting, TextComponent, Vault, htmlToMarkdown, normalizePath } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, ProgressBarComponent, Setting, TextComponent, htmlToMarkdown, normalizePath, requestUrl, sanitizeHTMLToDom } from 'obsidian';
+import { Readability } from 'readability';
 
 // @ts-ignore
 const electron = require("electron");
@@ -22,9 +23,6 @@ export default class SlurpPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// load readability script... in the sketchiest way possible :(
-		await this.loadScript();
 
 		this.addCommand({
 			id: 'replace-url',
@@ -58,19 +56,8 @@ export default class SlurpPlugin extends Plugin {
 		this.addCommand({
 			id: 'create-note-from-url',
 			name: 'Create note from URL',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SlurpNewNoteModal(this.app, this).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			callback: () => {
+				new SlurpNewNoteModal(this.app, this).open();
 			}
 		});
 	}
@@ -85,60 +72,34 @@ export default class SlurpPlugin extends Plugin {
 	async saveSettings() {
 	}
 
-	async loadScript() {
-		// TODO: check content hash and use specific commit in the url, add both to settings.
-		await fetch("https://raw.githubusercontent.com/mozilla/readability/main/Readability.js").then(async (resp) => {
-			this.readabilityScript = await resp.text();
-		}).catch(err => console.error(`Unable to load Readability: ${err}`));
-	}
-
 	async slurp(url: string, cb: (args: SlurpCallbackArgs) => void) {
-		const win = new electron.remote.BrowserWindow({ show: false });
+		const text = await requestUrl(url).text;
+		const doc = sanitizeHTMLToDom(text);
+		const article = new Readability(doc).parse();
 
-		win.loadURL(url).then(async () => {
-			// attempt to load the script again if it wasn't loaded at startup
-			if (!this.readabilityScript) {
-				await this.loadScript();
-			}
-			
-			// TODO: sanitize. it's sandboxed in the electron child window and all inputs will be from pages 
-			// 		 that the user has already loaded in their browser, so it should be safe enough for now.
-			//		 it wouldn't be the first time that i was wrong and dumb though!
-			const parseScript = this.readabilityScript + `
-				var article = new Readability(document).parse(); 
-				article;
-			`;
+		if (!article || !article.title || !article.content) {
+			console.error(`[Slurp] Parsed article missing critical content: ${article}.`);
+			cb({err: "No title or content found."});
+			return;
+		}
 
-			// inject Readability and extract content after the page has loaded
-			win.webContents.executeJavaScript(parseScript)
-				.then((article: any) => {
-					if (!article || !article.title || !article.content) {
-						console.error(`[Slurp] Parsed article missing critical content: ${article}.`);
-						cb({err: "No title or content found."});
-						return;
-					}
+		const md = htmlToMarkdown(sanitizeHTMLToDom(article.content));
+		if (!md) {
+			console.error(`[Slurp] Parsed content resulted in falsey markdown: ${md}`);
+			cb({err: "Unable to convert content to Markdown."});
+		}
 
-					const md = htmlToMarkdown(article.content);
-					if (!md) {
-						console.error(`[Slurp] Parsed content resulted in falsey markdown: ${md}`);
-						cb({err: "Unable to convert content to Markdown."});
-					}
+		let slurpArticle: SlurpArticle = {
+			title: article.title,
+			content: md,
+		};
 
-					let slurpArticle: SlurpArticle = {
-						title: article.title,
-						content: md,
-					};
+		if (article.excerpt) slurpArticle.excerpt = article.excerpt;
+		if (article.byline) slurpArticle.byline = article.byline;
+		if (article.siteName) slurpArticle.siteName = article.siteName;
+		if (article.publishedTime) slurpArticle.publishedTime = article.publishedTime;
 
-					if (article.excerpt) slurpArticle.excerpt = article.excerpt;
-					if (article.byline) slurpArticle.byline = article.byline;
-					if (article.siteName) slurpArticle.siteName = article.siteName;
-					if (article.publishedTime) slurpArticle.publishedTime = article.publishedTime;
-
-					cb({article: slurpArticle});	
-					win.close();
-				})
-				.catch((err: any) => console.error(`[Slurp] Failed to parse page: ${err}`));
-			});
+		cb({article: slurpArticle});
 	}
 }
 
@@ -197,9 +158,9 @@ class SlurpNewNoteModal extends Modal {
 		noteProps += "slurped: true\n";
 		noteProps += "---\n";
 
-		this.app.vault.create(filePath, noteProps + args.article.content)
-			.then((newFile) => this.app.workspace.getActiveViewOfType(MarkdownView)?.leaf.openFile(newFile));
-	}		
+		const newFile = await this.app.vault.create(filePath, noteProps + args.article.content);
+		this.app.workspace.getActiveViewOfType(MarkdownView)?.leaf.openFile(newFile);
+	}
 
 	onOpen() {
 		const { contentEl } = this;
